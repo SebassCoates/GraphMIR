d483bc73a0a2f4a50c5a234d93180cd127780172
Sebastian Coates <sebass@resnet152-237.medford.tufts.edu>
2018-05-05 17:37:55 -0400

    parsing working with basic quantize

--- utilities/miditograph.py
+++ utilities/miditograph.py
@@ -6,9 +6,21 @@ from sys import argv
 import os.path
 import numpy as np
 
+
+
 #quantize note lengths appropriately
-def quantize_note_length(note_length):
-    pass    
+def quantize(note_length):
+    lengths = [.03125, .0416, .0625, .0833, .125, .1667, .25, .3333, .5, .75,\
+                1, 1.25, 1.5, 1.75, 2, 2.5, 2.5, 2.75, 3, 3.5, 4, 4.5, 5, 6, 7,\
+                8, 9, 10, 12, 16,24]
+
+    diffs = np.zeros((len(lengths)))
+    for i, l in enumerate(lengths):
+        diffs[i] = abs(l - note_length)
+    closest = min(diffs)
+    #print(note_length, end=" ")
+    #print(list(diffs).index(closest), end=", ")
+    return list(diffs).index(closest)
 
 #parse note data from track to list of notes
 def parse_note_data(track, ticks_per_beat):
@@ -33,16 +45,8 @@ def parse_note_data(track, ticks_per_beat):
                 being_played[msg.note] = current_time 
             elif msg.velocity == 0 and msg.note in being_played: #note released
                 duration = current_time - being_played[msg.note]
-                note_data[current_chord - 1].append((msg.note, duration/ticks_per_beat))
+                note_data[current_chord - 1].append((msg.note, quantize(duration/ticks_per_beat)))
                 del being_played[msg.note]
-                    
-            #if msg.velocity == 0:
-            #    notes = []
-            #    for note in being_played:
-            #        notes.append(note)
-            #    being_played = set()
-            #    if notes != []:
-            #        note_data.append(notes)
         elif msg.type == 'note_off':
             pass
         elif msg.type == 'set_tempo':
@@ -56,22 +60,36 @@ def parse_note_data(track, ticks_per_beat):
     return note_data 
 
 def generate_graph(note_data):
-    notes = set()
+    notes = {} 
+    note_index = 0
     for chord in note_data:
         for note in chord:
-            print(note)
-            notes.add(note)
+            if note not in notes:
+                notes[note] = note_index
+                note_index += 1
     
     adjMatrix = np.zeros((len(notes), len(notes)))
+    
+    prev_notes = []
+    for chord in note_data:
+        for note in chord:
+            for prev in prev_notes:
+                adjMatrix[notes[prev]][notes[note]] += 1
+            for other in chord:
+                if note != other:
+                    adjMatrix[notes[note]][notes[other]] += 1
+
+        prev_notes = chord
     return adjMatrix
 
 def write_graph(adjMatrix, directory, filename):
-    fullname = os.path.join(directory, filename[:len(filename) - 3] + "grph")
+    fullname = os.path.join(directory, filename + ".grph")
     fullname = directory + "/" + filename
     outfile = open(fullname, 'w')
-    for row in adjMatrix:
-        for col in row:
-            outfile.write(str(adjMatrix[row][col]) + " ")
+    for row in range(len(adjMatrix)):
+        for col in range(len(adjMatrix)):
+            if adjMatrix[row][col] != 0:
+                outfile.write(str(col) + ',' + str(adjMatrix[row][col]) + " ")
         outfile.write("\n")
     outfile.close()
 
@@ -81,7 +99,7 @@ midi_files = []
 for arg in argv[1:]:
     if ".mid" in arg or ".MID" in arg:
         if len(midi_files) == 1:
-            continue #only consider 1 song for now
+            continue
         midi_files.append((MidiFile(arg), arg))
     elif "--dir=" in arg:
         output_directory = arg.replace("--dir=", "")
@@ -94,6 +112,7 @@ for i, midi_file in enumerate(midi_files):
     mid, name = midi_file
     merged = M.merge_tracks(mid.tracks)
     note_data.append(parse_note_data(mid.tracks[0], mid.ticks_per_beat))
-    adjMatrix = generate_graph(note_data)
+    adjMatrix = generate_graph(note_data[i])
     write_graph(adjMatrix, output_directory, str(i))
+    print('Wrote ' + name) 
 
